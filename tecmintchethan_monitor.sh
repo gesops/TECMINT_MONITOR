e2560d9ae426e0f81fb483d685bd32660710d58a
imchetanks <imchetanks@gmail.com>
2019-08-09 11:47:27 +0530

    Update and rename tecmint_monitor.sh to tecmintchethan_monitor.sh

--- tecmintchethan_monitor.sh
+++ tecmintchethan_monitor.sh
@@ -1,11 +1,3 @@
-                  ####################################################################################################
-                  #                                        Tecmint_monitor.sh                                        #
-                  # Written for Tecmint.com for the post www.tecmint.com/linux-server-health-monitoring-script/      #
-                  # If any bug, report us in the link below                                                          #
-                  # Free to use/edit/distribute the code below by                                                    #
-                  # giving proper credit to Tecmint.com and Author                                                   #
-                  #                                                                                                  #
-                  ####################################################################################################
 #! /bin/bash
 # unset any variable which system may be using
 
